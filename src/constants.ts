f55a19adbd08890e2d8ed65fc3adc42885f4adb0
linjay0208 <linjay0208@gmail.com>
2022-01-26 21:14:38 +0800

    swap-sdk-final

---
+++ src/constants.ts
@@ -0,0 +1,56 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 56,
+  TESTNET = 97
+}
+
+export enum RouterType {
+  sphynx = 'sphynx',
+  pancake = 'pancake'
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+// pancake-swap mainnet
+export const PANCAKE_FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+export const PANCAKE_INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
+
+// sphynx-swap mainnet
+export const SPHYNX_FACTORY_ADDRESS = '0xA57605ACfa74565E69DF1D38c2f13Bc4618F44b6'
+export const SPHYNX_INIT_CODE_HASH = '0x13007f1c7aacc1fbec57a53473b63ba8103ee4eb2060b98080477b6005cbfa49'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const FEES_NUMERATOR = JSBI.BigInt(9990)
+export const FEES_DENOMINATOR = JSBI.BigInt(10000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import JSBI from 'jsbi'
+export { JSBI }
+
+export {
+  BigintIsh,
+  ChainId,
+  RouterType,
+  TradeType,
+  Rounding,
+  PANCAKE_FACTORY_ADDRESS,
+  PANCAKE_INIT_CODE_HASH,
+  SPHYNX_FACTORY_ADDRESS,
+  SPHYNX_INIT_CODE_HASH,
+  MINIMUM_LIQUIDITY
+} from './constants'
+
+export * from './errors'
+export * from './entities'
+export * from './router'
+export * from './fetcher'
